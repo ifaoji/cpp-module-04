a0c7f0f5a13c9a6c145eac84813d52a91dff40c8
Simon Dablander <sdabland@student.42vienna.com>
2024-10-20 17:45:04 +0200

    fix(ex03): `Ice::clone` should actually clone (not important in this case though)

--- ex03/Ice.cpp
+++ ex03/Ice.cpp
@@ -17,7 +17,7 @@ Ice &Ice::operator=(const Ice &rhs) {
   return *this;
 }
 
-AMateria *Ice::clone() const { return new Ice; }
+AMateria *Ice::clone() const { return new Ice(*this); }
 
 void Ice::use(ICharacter &target) {
   std::cout << "* shoots an ice bolt at " << target.getName() << " *"
