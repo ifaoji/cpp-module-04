a837de6599900792af07715a0961a0791f08cd82
Simon Dablander <sdabland@student.42vienna.com>
2024-10-20 17:06:18 +0200

    feat(ex03): add more tests to `main`

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "AMateria.hpp"
 #include "Character.hpp"
 #include "Cure.hpp"
+#include "ICharacter.hpp"
+#include "IMateriaSource.hpp"
 #include "Ice.hpp"
 #include "MateriaSource.hpp"
 
@@ -13,6 +16,161 @@ void PrintHeader(const std::string &header) {
 }
 
 int main() {
+  {
+    PrintHeader("MatiaSource correct inventory handling");
+
+    AMateria *materias[5] = {new Ice(), new Ice(), new Cure(), new Ice(),
+                             new Cure()};
+    IMateriaSource *src = new MateriaSource();
+    for (int i = 0; i < 5; i++) {
+      src->learnMateria(materias[i]);
+    }
+
+    delete src;
+    // Materia source cannot hold the last item
+    delete materias[4];
+  }
+
+  {
+    PrintHeader("MatiaSource learn same materia");
+
+    AMateria *ice = new Ice();
+    IMateriaSource *src = new MateriaSource();
+    src->learnMateria(ice);
+    src->learnMateria(ice);
+    src->learnMateria(ice);
+    src->learnMateria(ice);
+
+    delete src;
+  }
+
+  {
+    PrintHeader("MatiaSource deep copy");
+
+    AMateria *materias[4] = {
+        new Ice(),
+        new Ice(),
+        new Cure(),
+        new Ice(),
+    };
+    IMateriaSource *src = new MateriaSource();
+    for (int i = 0; i < 4; i++) {
+      src->learnMateria(materias[i]);
+    }
+
+    IMateriaSource *src_copy = new MateriaSource(*(MateriaSource *)src);
+    MateriaSource src_copy2;
+    src_copy2 = *(MateriaSource *)src;
+
+    delete src;
+    delete src_copy;
+  }
+
+  {
+    PrintHeader("MatiaSource learn same materia");
+
+    AMateria *ice = new Ice();
+    ICharacter *me = new Character("me");
+    me->equip(ice);
+    me->equip(ice);
+    me->equip(ice);
+    me->equip(ice);
+
+    delete me;
+  }
+
+  {
+    PrintHeader("Character deep copy");
+
+    AMateria *materias[4] = {
+        new Ice(),
+        new Ice(),
+        new Cure(),
+        new Ice(),
+    };
+    ICharacter *me = new Character("me");
+    for (int i = 0; i < 4; i++) {
+      me->equip(materias[i]);
+    }
+
+    ICharacter *me_copy = new Character(*(Character *)me);
+    Character me_copy2;
+    me_copy2 = *(Character *)me;
+
+    delete me;
+    delete me_copy;
+  }
+
+  {
+    PrintHeader("Character unequip not freed");
+
+    AMateria *materias[4] = {
+        new Ice(),
+        new Ice(),
+        new Cure(),
+        new Ice(),
+    };
+    ICharacter *me = new Character("me");
+    for (int i = 0; i < 4; i++) {
+      me->equip(materias[i]);
+    }
+
+    me->unequip(1);
+
+    delete me;
+    delete materias[1];
+  }
+
+  {
+    PrintHeader("Character unequip");
+
+    AMateria *materias[4] = {
+        new Ice(),
+        new Ice(),
+        new Cure(),
+        new Ice(),
+    };
+
+    AMateria *other = new Cure();
+    ICharacter *me = new Character("me");
+    for (int i = 0; i < 4; i++) {
+      me->equip(materias[i]);
+    }
+
+    me->unequip(1);
+    me->equip(other);
+
+    delete me;
+    delete materias[1];
+  }
+
+  {
+    PrintHeader("Character unequip correct ordering");
+
+    AMateria *materias[4] = {
+        new Ice(),
+        new Ice(),
+        new Cure(),
+        new Ice(),
+    };
+
+    ICharacter *me = new Character("me");
+    for (int i = 0; i < 4; i++) {
+      me->equip(materias[i]);
+    }
+
+    me->unequip(3);
+    me->unequip(2);
+    me->unequip(1);
+    me->unequip(0);
+
+    delete me;
+    delete materias[0];
+    delete materias[1];
+    delete materias[2];
+    delete materias[3];
+  }
+
   {
     PrintHeader("Subject");
 
